9f7a5d043dc9efc20c3829fbfbe8e7a75767471f
MrLudo85 <39162338+MrLudo85@users.noreply.github.com>
2018-05-10 16:26:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("%")
+
+bot.login("NDQ0MTI4NzY4MDE0MDI0NzI1.DdXecQ.Q4hlUKvZnpBDRghE33zu4bHfR0Y");
+
+
+bot.on('ready', function() {
+    bot.user.setActivity(`DarK Bot | %aide | ${bot.guilds.size} serveur | en developement `, {type: "STREAMING"});
+    console.log("Je suis connecter, le bot est maintenant en ligne et repond au commands.");
+});
+
+bot.on('message', message => {
+
+    if(message.content === prefix + "serverinfo") {
+        var serverinfo = new Discord.RichEmbed()
+        .setDescription("Infomation du serveur")
+        .addField("Non du discord :", message.guild.name)
+        .addField("Crée le :", message.guild.createdAt)
+        .addField("Tu as rejoin le :", message.member.createdAt)
+        .addField("Nombre de joueur sur le serveur :", message.guild.memberCount)
+        .setColor("0x0000FF")
+    message.channel.sendEmbed(serverinfo)
+
+    }
+
+bot.on('message', message => {
+
+    if (command === prefix + "aide") {
+
+        var aideEmbed = new Discord.RichEmbed()
+        .setTitle(":book: Voici la page d'aide du bot")
+        .setColor("#FF0105")
+        .addField("d/aide", "affiche cette page donc la page d'aide du serveur")
+        .addField("▬▬▬▬▬MODERATION▬▬▬▬▬", "moderation")
+        .addField("d/kick", "permet de kick l'utilisateur")
+        .addField("d/ban", "permet de ban un utilisateur")
+        .addField("▬▬▬▬▬GENERAL▬▬▬▬▬", "general")
+        .addField("d/invite", "donne le lien d'invitation du bot")
+        .addField("d/support", "donne le lien du support du bot")
+        .addField("d/site", "donne le lien du site internet du fondateur")
+        .addField("d/prefix", "donne le prifx du serveur + bientot custom prefix")
+        .addField("d/info", "donne les informations du bot")
+        .addField("d/stats", "donne les statistiques de l'utilisateur")
+        .addField("d/say", "permes au bot de repeter votre message")
+        .addField("d/avatar", "donne l'avatar de l'utilisateur")
+        message.channel.sendEmbed(aideEmbed);
+    }})});
