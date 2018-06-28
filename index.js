d5ba865969c368f0f7903f3c0821aec6eb29a130
MrLudo85 <39162338+MrLudo85@users.noreply.github.com>
2018-06-28 23:00:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -737,4 +737,47 @@ bot.on("message", message => {
         bot.shutdown(true);
 
     };
+        
+    if (message.content.startsWith(prefix + "eval")) {
+            const config = require('./config.json');
+            if(config.maintainers.indexOf(message.author.id)> -1 ) {
+                var util = require("util");
+                let args = message.content.split(" ").slice(1); 	
+                let code = args.join(' ');
+					try {
+			  let ev = eval(code)
+							let str = util.inspect(ev, {
+								depth: 1
+							})
+							if(str.length > 1800) {
+								str = str.substr(0, 1800)
+								str = str + "..."
+							}
+							message.delete();	
+			message.react("✅");
+					message.channel.send("", { embed: {	
+						color: 16758725,			
+				fields: [{				
+					name: ':inbox_tray: **Input**',			
+						value: '\`\`\`' + code + '\`\`\`'					
+			},{			
+						name: ':outbox_tray: **Output**',	
+								value: '\`\`\`' + str + '\`\`\`'	
+							}],	
+						footer: {			
+					text: `request by @${message.author.username}`		}			}});}	catch (err) {		message.react("❌");
+			message.channel.send("", { embed: {	
+						color: 16758725,			
+				fields: [{				
+					name: ':inbox_tray: **Input**',			
+						value: '\`\`\`' + code + '\`\`\`'					
+			},{			
+						name: ':outbox_tray: **Output**',	
+								value: '\`\`\`' + err + '\`\`\`'	
+							}],	
+						footer: {			
+                    text: `request by @${message.author.username}`		}			}});		}	}else{
+                        return message.reply("Tu n'est pas **l'Owner**")
+                    }
+        }
 }});
